3783d982952760317deedab04e34cdb384756964
sanpalma22 <spama0610@gmail.com>
2024-03-23 13:18:42 -0300

    terminado

--- codigo.js
+++ codigo.js
@@ -1,16 +1,21 @@
 let tareas=[
-{ nombre: "Tarea matematica", hecho: true, fechaIngreso: new Date(0),fechaTachado:new Date()}, { nombre: "Tarea lengua", hecho:true, fechaIngreso: new Date(0),fechaTachado:new Date()}
+{ nombre: "Tarea matematica", hecho: false, fechaIngreso: new Date(0),fechaTachado:null}, { nombre: "Tarea lengua", hecho:false, fechaIngreso: new Date(0),fechaTachado:null}
 ];
 
 function mostrarLista(tareas){
     let html=""
     tareas.forEach((t,index)=>{
         let fecha=`${t.fechaIngreso.getDate()}/${t.fechaIngreso.getMonth()+1}/${t.fechaIngreso.getFullYear()}  ${t.fechaIngreso.getHours()}:${t.fechaIngreso.getMinutes()}`;
-        if(t.hecho==true) html+=`<li style="text-decoration:line-through"; onclick="tacharTarea(${index})">${t.nombre}</li>`;
-        else html+=`<li onclick="tacharTarea(${index})">${t.nombre}</li>`;
+        if(t.hecho==true) {
+            html+=`<div class="tarea-hecha" >`;
+            html+=`<li style="text-decoration:line-through" onclick="tacharTarea(${index})">${t.nombre}</li>`;
+        } else {
+            html+=`<div class="tarea-pendiente" >`;
+            html+=`<li onclick="tacharTarea(${index})">${t.nombre}</li>`;
+        }
 
-        html+=`<p>${fecha}</p>
-        <button onclick="borrarTarea(${index})">Borrar tarea</button>`;
+        html+=`<p class="fecha">${fecha}</p>
+        <button class="tachar" onclick="borrarTarea(${index})"><img src="https://cdn-icons-png.flaticon.com/512/3817/3817209.png" ></button></div>`;
     })
     document.getElementById("listaTareas").innerHTML= html;
 }
@@ -40,7 +45,7 @@ document.getElementById("ingreso").onclick=()=>{
 document.getElementById("tareaMasRapida").onclick=()=>{
     const tareasTachadas=tareas.filter(t=>t.hecho==true);
     let tareaR=tareasTachadas[0];
-    if(tareasTachadas.length==0)alert("No hay tareas tachadas");
+    if(tareasTachadas.length==0)alert("No hay tareas hechas");
     tareasTachadas.forEach(t=>{
         if(t.fechaTachado-t.fechaIngreso<tareaR.fechaTachado-tareaR.fechaIngreso) tareaR=t
     });
